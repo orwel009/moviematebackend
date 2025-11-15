9f788b4dcb3cd61e5d9e4e3cbdde56e271b2cc72
orwel009 <orwelvincent1@gmail.com>
2025-11-15 16:17:50 +0530

    deploy commit

--- start.sh
+++ start.sh
@@ -11,4 +11,4 @@ echo "Collecting static files..."
 python manage.py collectstatic --noinput
 
 echo "Starting Gunicorn..."
-exec gunicorn backend.wsgi:application --bind 0.0.0.0:${PORT:-8000} --workers 3
+exec gunicorn moviematebackend.wsgi:application --bind 0.0.0.0:${PORT:-8000} --workers 3
